9d516d3c179b8de48d87ecbe46cd0f54b5c025e7
luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-02-10 11:04:04 -0300

    first front-end exercise

---
+++ aula1/App.js
@@ -0,0 +1,21 @@
+function nãosei(params) {
+    
+    let obj = {nome:"Luiz miguel santos almeida",
+               idade:18,
+               soma :  7+3,
+
+
+
+               
+    }
+
+    console.log(obj.nome);
+    console.log(obj.idade);
+    console.log(obj.soma);
+        
+
+
+    
+}
+nãosei();
+
