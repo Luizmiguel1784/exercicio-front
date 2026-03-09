a9c2295f809c8d530b3a035333dc780c1677b66c
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-03-09 16:07:46 -0300

    fifth javascript exercise

---
+++ Exercicio 5/app.js
@@ -0,0 +1,15 @@
+for (let index = 1; index < 21; index++) {
+    console.log(index);
+    
+}
+
+let contador = 2
+while (contador < 20) {
+   contador = contador + 2
+
+   console.log(contador);
+
+}
+do {
+    let senha = prompt("Digite senha: ")
+} while (senha == 1234);
