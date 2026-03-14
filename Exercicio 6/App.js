bcc2601a8e263768fa1c7dccd66b8d2912367368
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-03-14 13:01:01 -0300

    sixth javascript exercise

---
+++ Exercicio 6/App.js
@@ -0,0 +1,20 @@
+//Função tradicional
+function saudacao(nome, idade) {
+
+    console.log(`Olá ${nome}, vc tem ${idade}`);   
+    
+}
+//Função anônima
+const dobro = function(a) {
+    return a * 2
+}
+
+//Arrow function
+const soma =  (b,c) => b + c;
+
+
+saudacao("Luiz", 18)
+
+console.log( "Dobro de 123:", dobro(123));
+
+console.log("Soma:", soma(5,7));
