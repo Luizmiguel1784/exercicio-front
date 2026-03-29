56e94cec6604b4e9fbfdc8843358f3e8a15242ff
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-03-28 22:50:30 -0300

    feat: create modal file

---
+++ reactComponent/src/assets/components/Task/Modal/modal.jsx
@@ -0,0 +1,25 @@
+import styles from './Modal.module.css';
+
+export default function Modal({ isOpen, onClose, children }) {
+  
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.overlay}>
+      
+      <div className={styles.modal}>
+        
+        <button 
+          className={styles.close} 
+          onClick={onClose}
+        >
+          X
+        </button>
+
+        {children}
+
+      </div>
+
+    </div>
+  );
+}
