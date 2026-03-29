e4676ff71918b368202ec9852e45204dbaccfc3a
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-03-28 22:51:25 -0300

    feat: create timer

--- reactComponent/src/assets/components/Timer.jsx
+++ reactComponent/src/assets/components/Timer.jsx
@@ -1,20 +1,60 @@
-import { useState } from "react"
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
+const Container = styled.div`
+
+padding: 20px;
+  text-align: center;
+
+`;
+
+const Button =styled.div`
+  padding: 10px 20px;
+  margin-top: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
 export default function Timer() {
     
     const [Time,setTime] = useState(0);
+    const [rodando, setRodando] = useState(false);
+
+    useEffect(()=> {
+    let interval;
+
+    if(rodando){
+    interval= setInterval(()=> {
+    setTime((prev)=> prev+1)},1000);}
+
+    return()=> clearInterval(interval);
+    }
+    ,[rodando])
+    
 
     return (
         <Container>
             <h2>Tempo: {Time}</h2>
 
-            <Button onClick ={()=> setTime(Time+1)}>
-                Aumentar tempo
+            <Button onClick ={()=> setRodando(true)}>
+                Iniciar
             </Button>
 
+        
+            <Button onClick ={()=> setRodando(false)}>
+                Pausar
+            </Button>
 
+            
+            <Button onClick ={()=> {setRodando(false)
+            setTime(0);
+            }}>
+                Resetar
+            </Button>
         </Container>
 
-    )
+    );
+
 
 }
