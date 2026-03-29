438c74d44025d08530bf47f1245e3be74ff63857
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-03-28 22:54:14 -0300

    feat : create app structure

--- reactComponent/src/App.jsx
+++ reactComponent/src/App.jsx
@@ -1,16 +1,19 @@
-import Header from "./assets/components/Header"
-import TaskList from "./assets/components/TaskList"
-import Timer from "./assets/components/Timer"
-export default App 
-function App() {
-  
+import './style.css';
+import Header from './assets/components/Header/Header.jsx';
+import TaskList from './assets/components/Task/TaskList.jsx';
+import Timer from './assets/components/Timer.jsx';
 
-  return( 
-    <>
-    <h1>OI</h1>
-    <Header/>
-    <TaskList/>
-    <Timer/>
-  </>
-  )
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="main">
+        <TaskList />
+        <Timer />
+      </div>
+    </div>
+  );
 }
+
+export default App;
